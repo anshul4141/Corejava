0c4dbffbe27c587b1a1008f0f7d2badf5aebd9f8
anshul4141 <anshulprajapati691@gmail.com>
2022-07-04 12:18:07 +0530

    message

--- oopexercise/StaticPolymorphism.java
+++ oopexercise/StaticPolymorphism.java
@@ -1,27 +1,25 @@
 package com.raystec.oopexercise;
 
 public class StaticPolymorphism {
-	public void add(int a, int b) {
+	public static void add(int a, int b) {
 		System.out.println(a+b);
 		
-	}public void add(int a, int b, int c) {
+	}public static void add(int a, int b, int c) {
 		System.out.println(a+b+c);
 		
-	}public void add(int a, int b, int c, int d) {
+	}public static void add(int a, int b, int c, int d) {
 		System.out.println(a+b+c+d);
 		
-	}public void add(String c) {
+	}public static void add(String c) {
 		System.out.println(c);
 		
 	}
 	public static void main(String[] args) {
 		
-		StaticPolymorphism obj = new StaticPolymorphism();
-		
-		obj.add(50, 25);
-		obj.add(25, 72, 63);
-		obj.add(25, 15, 13, 72);
-		obj.add("Anshul");
+		StaticPolymorphism.add(50, 25);
+		StaticPolymorphism.add(25, 72, 63);
+		StaticPolymorphism.add(25, 15, 13, 72);
+		StaticPolymorphism.add("Anshul");
 	}
 	
 		
--- oopexercise/TotalArea.java
+++ oopexercise/TotalArea.java
@@ -1,6 +1,5 @@
 package com.raystec.oopexercise;
 
-
 public class TotalArea {
 	
 	public static void main(String[] args) {
@@ -10,7 +9,7 @@ public class TotalArea {
 		s1[1] = new Triangle();
 		s1[2] = new Circle();
 
-		Rectangle r = (Rectangle) s1[0];
+		Rectangle r = (Rectangle) s1[0];           
 		r.setLength(5);
 		r.setWidth(5);
 
@@ -26,12 +25,8 @@ public class TotalArea {
 			double d = s1[i].area();
 			System.out.println(d); 
 			total = total + s1[i].area();
-
-		}
+        }
 		System.out.println("{Total Area} = " + total);
-		
-
-		
-	}
-
+		}
 }
+
